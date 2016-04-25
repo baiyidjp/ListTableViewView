75586cf5ee47c96a5aa440c3f5ea328d468c8fac
baiyidjp <tztddong@dongjiangpengdemac-mini.local>
2016-04-25 17:35:35 +0800

    Initial Commit

---
+++ customBtn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  customBtn
+//
+//  Created by tztddong on 16/4/25.
+//  Copyright © 2016年 dongjiangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
